df690ef3caac07ab2c9fc29230f943406bcbdedf
Yashodha L <136436391+Dhanya9706@users.noreply.github.com>
2023-06-15 14:08:19 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,1 +1,4 @@
-pytest -vsm 'sanity or regression' --html=./Reports/testlogin.html D:\Project\SeleniumHandsonProject\TestCases\ --browser chrome
+python -m pytest -vsm sanity --html=./Reports/testloginchrome.html D:\Project\SeleniumHandsonProject\TestCases\ --browser chrome
+python -m pytest -vsm regression --html=./Reports/testloginchromeheadless.html D:\Project\SeleniumHandsonProject\TestCases\ --browser chromeheadless
+python -m pytest -vsm sanity --html=./Reports/testloginedge.html D:\Project\SeleniumHandsonProject\TestCases\ --browser edge
+python -m pytest -vsm regression --html=./Reports/testloginedgeheadless.html D:\Project\SeleniumHandsonProject\TestCases\ --browser edgeheadless
