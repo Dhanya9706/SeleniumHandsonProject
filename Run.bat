cbb7b436941edb44e9023371e0819d295680d2b7
Yashodha L <136436391+Dhanya9706@users.noreply.github.com>
2023-06-15 14:23:07 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,4 +1,4 @@
-python -m pytest -vsm sanity --html=./Reports/testloginchrome.html D:\Project\SeleniumHandsonProject\TestCases\ --browser chrome
-python -m pytest -vsm regression --html=./Reports/testloginchromeheadless.html D:\Project\SeleniumHandsonProject\TestCases\ --browser chromeheadless
-python -m pytest -vsm sanity --html=./Reports/testloginedge.html D:\Project\SeleniumHandsonProject\TestCases\ --browser edge
-python -m pytest -vsm regression --html=./Reports/testloginedgeheadless.html D:\Project\SeleniumHandsonProject\TestCases\ --browser edgeheadless
+python -m pytest -vsm sanity --html=./Reports/testloginchrome.html D:\Project\SeleniumHandsonProject\TestCases --browser chrome
+python -m pytest -vsm regression --html=./Reports/testloginchromeheadless.html D:\Project\SeleniumHandsonProject\TestCases --browser chromeheadless
+python -m pytest -vsm sanity --html=./Reports/testloginedge.html D:\Project\SeleniumHandsonProject\TestCases --browser edge
+python -m pytest -vsm regression --html=./Reports/testloginedgeheadless.html D:\Project\SeleniumHandsonProject\TestCases --browser edgeheadless
